345dbdbe63168b08f81e6baf78248d0989da216d
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-10 19:48:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import session from 'express-session'
+
+const app = express()
+const server = app.listen(8080, () => console.log('Server Up'))
+
+let baseSession = session({
+    secret: 'c0d3r',
+    resave: false,
+    saveUninitialized: false
+})
+
+app.use(express.json())
+app.use(baseSession)
+app.use(express.static('public'))
+
+let users = []
